caeef78ad3073faf7ea631f94ea8dff05ad65b23
Nischal Hada <hadanischal@gmail.com>
2019-08-25 13:57:39 +1000

    Implemented CategoriesTableViewController, viewmodel, viewmodeltest

---
+++ NewsArticle/Cells/CategoriesTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  CategoriesTableViewCell.swift
+//  NewsArticle
+//
+//  Created by Nischal Hada on 24/8/19.
+//  Copyright © 2019 NischalHada. All rights reserved.
+//
+
+import UIKit
+
+class CategoriesTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ NewsArticleTests/CategoriesViewModelTests.swift
@@ -0,0 +1,92 @@
+//
+//  CategoriesViewModelTests.swift
+//  NewsArticleTests
+//
+//  Created by Nischal Hada on 25/8/19.
+//  Copyright © 2019 NischalHada. All rights reserved.
+//
+
+import XCTest
+import Quick
+import Nimble
+import Cuckoo
+import RxTest
+import RxBlocking
+import RxSwift
+
+@testable import NewsArticle
+
+class CategoriesViewModelTests: QuickSpec {
+    
+    override func spec() {
+        var testViewModel: CategoriesViewModel!
+        var mockCategoriesHandler: MockCategoriesHandlerProtocol!
+        var testScheduler: TestScheduler!
+        let mockCategories = [
+            "Business",
+            "Entertainment",
+            "Health",
+            "Science",
+            "Sports",
+            "Technology"
+        ]
+        //     "categories":
+        
+        let mockCategory = CategoriesModel(categories: mockCategories)
+        
+        describe("CategoriesViewModel") {
+            beforeEach {
+                testScheduler = TestScheduler(initialClock: 0)
+                mockCategoriesHandler = MockCategoriesHandlerProtocol()
+                stub(mockCategoriesHandler, block: { stub in
+                    when(stub.getCategories()).thenReturn(Observable.empty())
+                })
+                testViewModel = CategoriesViewModel(withCategoriesHandler: mockCategoriesHandler)
+            }
+            
+            describe("Get news info on initialization", {
+                context("when get news succeed ", {
+                    beforeEach {
+                        stub(mockCategoriesHandler, block: { stub in
+                            when(stub.getCategories()).thenReturn(Observable.just(mockCategory))
+                        })
+                    }
+                    it("it completed successfully", closure: {
+                        _ = testViewModel.getCategories().asObservable()
+                        verify(mockCategoriesHandler).getCategories()
+                    })
+                    it("emits the news list to the UI", closure: {
+                        
+                        let res = testScheduler.start { testViewModel.getCategories().asObservable() }
+                        expect(res.events.count).to(equal(2))
+                        let correctResult = [Recorded.next(200, mockCategory),
+                                             Recorded.completed(200)]
+                        expect(res.events).to(equal(correctResult))
+                    })
+                })
+                
+                
+                context("when get news failed ", {
+                    let mockError: Error =  RxError.noElements
+                    
+                    beforeEach {
+                        stub(mockCategoriesHandler, block: { stub in
+                            when(stub.getCategories()).thenReturn(Observable.error(mockError))
+                        })
+                    }
+                    it("it completed successfully", closure: {
+                        _ = testViewModel.getCategories().asObservable()
+                        verify(mockCategoriesHandler).getCategories()
+                    })
+                    it("emits the news list to the UI", closure: {
+                        let res = testScheduler.start { testViewModel.getCategories().asObservable() }
+                        expect(res.events.count).to(equal(1))
+//                        let correctResult = [Recorded.error(200, mockError),
+//                                             Recorded.completed(200)]
+//                        expect(res.events).to(equal(correctResult))
+                    })
+                })
+            })
+        }
+    }
+}
