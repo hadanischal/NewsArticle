59602be3a030f0bc9d5be64abaa10743a081ab6c
Nischal Hada <hadanischal@gmail.com>
2020-07-24 10:15:33 +1000

    [UnitTest Utility] Added unit test utility

---
+++ NewsArticleTests/Utility/MaterializedSequenceResult.swift
@@ -0,0 +1,47 @@
+//
+//  MaterializedSequenceResult.swift
+//  NewsArticleTests
+//
+//  Created by Nischal Hada on 21/7/20.
+//  Copyright © 2020 NischalHada. All rights reserved.
+//
+
+import RxBlocking
+import XCTest
+
+extension MaterializedSequenceResult {
+    /// A convenience method for testing that a stream has ended with a failure
+    /// - parameter error: The expected error that will be thrown by the stream
+    /// Replaces the longer switch statement checking
+    func assertSequenceDidFail<T: Error & Equatable>(withError expectedError: T) {
+        switch self {
+        case .completed:
+            XCTFail("Expected result to complete with error, but result was successful.")
+        case let .failed(_, error):
+            XCTAssertEqual(error as? T, expectedError)
+        }
+    }
+
+    /// A convenience method for testing that a stream has ended with a failure
+    /// Replaces the longer switch statement checking
+    func assertSequenceDidFail() {
+        switch self {
+        case .completed:
+            XCTFail("Expected result to complete with error, but result was successful.")
+        case let .failed(_, error):
+            XCTAssertNotNil(error)
+        }
+    }
+
+    /// A convenience method for testing that a stream has completed
+    /// Replaces the longer switch statement checking
+    func assertSequenceCompletes() {
+        switch self {
+        case .completed:
+        break // pass the test
+        case let .failed(_, error):
+            XCTFail("Expected result to complete without error, but received \(error).")
+            XCTFail("error occured \(error)")
+        }
+    }
+}
