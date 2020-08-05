e40c45edf40ef8063a1145b8ead729e3e91cf5f4
Nischal Hada <hadanischal@gmail.com>
2020-08-05 21:16:37 +1000

    [Refactored] Code refactored

--- NewsArticleTests/NewsArticleTests.swift
+++ NewsArticleTests/NewsArticleTests.swift
@@ -6,11 +6,10 @@
 //  Copyright © 2019 NischalHada. All rights reserved.
 //
 
-import XCTest
 @testable import NewsArticle
+import XCTest
 
 class NewsArticleTests: XCTestCase {
-
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
@@ -30,5 +29,4 @@ class NewsArticleTests: XCTestCase {
             // Put the code you want to measure the time of here.
         }
     }
-
 }
